f5aa66e1f1ae55e6ddaad4bbc8abf0e1bf3980cd
nikit <nikita.nikita@gmail.com>
2022-11-23 18:16:32 +0530

    Snake and Ladder game played with single player at start position 0

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -12,24 +12,6 @@ namespace SnakeAndLadderProblem
         {
             UC1Position.Position();
             Console.ReadLine();
-
-            UC2Roll.Roll();
-            Console.ReadLine();
-
-            UC3NoPlay.NoPlay();
-            Console.ReadLine();
-
-            UC4Repeat.Repeat();
-            Console.ReadLine();
-
-            UC5Winning.Winning();
-            Console.ReadLine();
-
-            UC6Report.Report();
-            Console.ReadLine();
-
-            UC7TwoPlayer.TwoPlayer();
-            Console.ReadLine();
         }
     }
 }
